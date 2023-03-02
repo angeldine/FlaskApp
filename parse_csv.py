8c15494288b455f1af710b34935364efa8ecf9c6
BERNADINE AMAKA EMMANUEL <b.emmanuel.22@abdn.ac.uk>
2023-03-02 23:52:26 +0000

    my first project

--- parse_csv.py
+++ parse_csv.py
@@ -10,12 +10,15 @@ cur = conn.cursor()
 conn.execute('DROP TABLE IF EXISTS orders')
 conn.execute('DROP TABLE IF EXISTS customers')
 conn.execute('DROP TABLE IF EXISTS products')
+conn.execute('DROP TABLE IF EXISTS links')
+
 print("tables dropped successfully")
 
 # create table again
-conn.execute('CREATE TABLE products (Id INTEGER PRIMARY KEY AUTOINCREMENT, Product_Id TEXT, Product_Name TEXT, Price REAL, Quantity INTEGER)')
-conn.execute('CREATE TABLE customers (Id INTEGER PRIMARY KEY AUTOINCREMENT, Customer_Id TEXT, Customer_Name TEXT, Segment TEXT, Country TEXT)')
+conn.execute('CREATE TABLE products (Id INTEGER PRIMARY KEY AUTOINCREMENT, Product_Id TEXT, Product_Name TEXT, Category TEXT, Subcategory TEXT, Price REAL, Quantity INTEGER)')
+conn.execute('CREATE TABLE customers (Id INTEGER PRIMARY KEY AUTOINCREMENT, Customer_Id TEXT, Customer_Name TEXT, Segment TEXT, city TEXT, state TEXT, postal TEXT, Country TEXT)')
 conn.execute('CREATE TABLE orders (Id INTEGER PRIMARY KEY AUTOINCREMENT,  Order_Id TEXT, customer_id INTEGER, Order_Date TEXT, Ship_Date TEXT, Ship_Mode TEXT, FOREIGN KEY(customer_id) REFERENCES customers(Id))')
+conn.execute('CREATE TABLE links (id INTEGER PRIMARY KEY AUTOINCREMENT, customer_id TEXT, customer_name TEXT, order_id TEXT, order_date TEXT, product_id TEXT, product_name TEXT, cost REAL, quantity INTEGER, FOREIGN KEY(customer_id) REFERENCES customers(Customer_Id), FOREIGN KEY(order_id) REFERENCES orders(Order_Id), FOREIGN KEY(product_id) REFERENCES products(Product_Id))')
 print("tables created successfully")
 
 
@@ -29,17 +32,19 @@ with open('data.csv', newline='') as f:
       # print(p)
       Product_Id = p[13]
       Product_Name = p[16]
+      Category = p[14]
+      Subcategory = p[15]
       Price = float(p[17])
       Quantity = int(p[18])
 
-      cur.execute('INSERT INTO products VALUES (NULL,?,?,?,?)', (Product_Id, Product_Name, Price, Quantity))
+      cur.execute('INSERT INTO products VALUES (NULL,?,?,?,?,?,?)', (Product_Id, Product_Name, Category, Subcategory, Price, Quantity))
       conn.commit()
    print("data parsed successfully")
 
 
 
 
-with open('data.csv', newline='') as f:
+with open('us_superstore.csv', newline='') as f:
    reader = csv.reader(f, delimiter=",")
    next(reader) # skip the header line
 
@@ -50,11 +55,14 @@ with open('data.csv', newline='') as f:
       Customer_Id = c[5]
       Customer_Name = c[6]
       Segment = c[7]
+      city = c[9]
+      state = c[10]
+      postal = c[11]
       Country = c[8]
-      cur.execute('INSERT INTO customers VALUES (NULL,?,?,?,?)', (Customer_Id, Customer_Name, Segment, Country))
+      cur.execute('INSERT INTO customers VALUES (NULL,?,?,?,?,?,?,?)', (Customer_Id, Customer_Name, Segment, city, state, postal, Country))
       conn.commit()
 
-with open('data.csv', newline='') as f:
+with open('us_superstore.csv', newline='') as f:
    reader = csv.reader(f, delimiter=",")
    next(reader) # skip the header line
    
@@ -70,12 +78,30 @@ with open('data.csv', newline='') as f:
       cur.execute('INSERT INTO orders VALUES (NULL,?,?,?,?,?)', (Order_Id, customer_id, Order_Date, Ship_Date, Ship_Mode))
       conn.commit()
    print("data parsed successfully")
-  
-   
+
+
+with open('us_superstore.csv', newline='') as f:
+   reader = csv.reader(f, delimiter=",")
+   next(reader) # skip the header line
+
+   for link in reader:
+      order_id = link[1]
+      order_date = link[2]
+      customer_id = link[5]
+      customer_name = link[6]
+      product_id = link[13]
+      product_name = link[16]
+      cost = float(link[17])
+      quantity = int(link[18])
+      cur.execute('INSERT INTO links VALUES (NULL,?,?,?,?,?,?,?,?)', (customer_id, customer_name, order_id, order_date, product_id, product_name, cost, quantity))
+      conn.commit()
+   print("data parsed successfully")
+
+
          
 
          
-   
+      
 
    
 
--- superstore_data.py
+++ superstore_data.py
@@ -1,39 +1,77 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, g, render_template
 
 app = Flask(__name__)
 
+app.config.from_object(__name__)
+
+# database details - to remove some duplication
+db_name = 'us_superstore_data.db'
+
+# code based on example at https://github.com/mjhea0/flaskr-tdd
+
+def connect_db():
+    conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+    
+
+@app.route('/orders')
+def orders():
     # open the connection to the database
-    conn = sqlite3.connect('us_superstore_data.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute("select * FROM orders limit 20")
+    db = get_db()
+    cur = db.execute("select * FROM orders limit 30")
     rows = cur.fetchall()
-    conn.close()
-    return render_template('index.html', rows=rows)
+    return render_template('orders.html', rows=rows)
 
 
 @app.route('/customers')
 def customers():
     # open the connection to the database
-    conn = sqlite3.connect('us_superstore_data.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute("select * FROM customers limit 20")
+    db = get_db()
+    cur = db.execute("select * FROM customers limit 30")
     customer = cur.fetchall()
-    conn.close()
     return render_template('customers.html', customer=customer)
 
 
 @app.route('/products')
 def products():
     # open the connection to the database
-    conn = sqlite3.connect('us_superstore_data.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute("select * FROM products limit 20")
+    db = get_db()
+    cur = db.execute("select * FROM products limit 30")
     product = cur.fetchall()
-    conn.close()
     return render_template('products.html', product=product)
+
+
+@app.route('/customer_details/<id>')
+def customer_details(id):
+    db = get_db()
+    # cur.execute("SELECT customers.Customer_Id, orders.Order_Id, customers.Customer_Name FROM orders, customers WHERE orders.customer_id = customers.Customer_Id AND customers.Customer_Id = ?", (id,))
+    cur = db.execute("SELECT * from links WHERE customer_id = ?", (id,))
+    cus = cur.fetchall()
+    return render_template('customer_details.html', customer=cus)
+
+
+@app.route('/order_details/<id>')
+def order_details(id):
+    db = get_db()
+    # cur.execute("SELECT customers.Customer_Id, orders.Order_Id, customers.Customer_Name FROM orders, customers WHERE orders.customer_id = customers.Customer_Id AND customers.Customer_Id = ?", (id,))
+    cur = db.execute("SELECT * from links WHERE order_id = ?", (id,))
+    cus = cur.fetchall()
+    return render_template('order_details.html', order=cus)
+
